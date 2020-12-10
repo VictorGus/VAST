fd2175473b3a804323183f9b22f2a166966406f4
VictorGus <retaow@gmail.com>
2020-12-10 16:11:42 +0300

    display factories and monitors from db

--- ui/src/app/map/model.cljc
+++ ui/src/app/map/model.cljc
@@ -3,10 +3,33 @@
 
 (def index ::index)
 
+(def mini-db (atom {}))
+
 (rf/reg-sub
  index
  (fn [db]
-   db))
+   (get-in db [index :data])))
+
+
+(rf/reg-event-fx
+ ::retrieve-monitors
+ (fn [{db :db} _]
+   {:xhr/fetch {:uri "/monitor"
+                :success {:event ::save-data
+                          :params {:type :monitors}}}}))
+
+(rf/reg-event-fx
+ ::retrieve-factories
+ (fn [{db :db} _]
+   {:xhr/fetch {:uri "/factory"
+                :success {:event ::save-data
+                          :params {:type :factories}}}}))
+
+(rf/reg-event-fx
+ ::save-data
+ (fn [{db :db} [_ {{{data :data} :entry} :data {{{:keys [type]} :params} :success} :request}]]
+   (swap! mini-db assoc type data)
+   {:db (assoc-in db [index :data type] data)}))
 
 (rf/reg-event-fx
  index
@@ -15,4 +38,5 @@
      (= phase :deinit)
      {}
      (= phase :init)
-     {:db db})))
+     {:dispatch-n [[::retrieve-monitors]
+                   [::retrieve-factories]]})))
--- ui/src/app/map/view.cljs
+++ ui/src/app/map/view.cljs
@@ -7,12 +7,22 @@
             [app.map.model :as model])
   (:require-macros [hiccup.core :as hc]))
 
+(defn normalize-coords [hm]
+  (-> hm
+      (assoc :latitude  (js/parseFloat (:latitude  hm)))
+      (assoc :longitude (js/parseFloat (:longitude hm)))))
 
 (defn map-page [data]
   (r/create-class
    {:component-did-mount
     (fn [this]
-      (let [usa-map (js/Datamap. (clj->js {:element (.getElementById js/document "container")
+      (let [factory-bubble {:radius 10
+                            :fillOpacity 2.0
+                            :fillKey "FACTORY"}
+            monitor-bubble {:radius 6
+                            :fillOpacity 2.0
+                            :fillKey "MONITOR"}
+            usa-map (js/Datamap. (clj->js {:element (.getElementById js/document "container")
                                            :scope "usa"
                                            :geographyConfig {:borderColor "black"
                                                              :borderWidth "1"
@@ -22,66 +32,18 @@
                                            :fills {:defaultFill "white"
                                                    "FACTORY" "#ff7f0e"
                                                    "MONITOR" "#3f8c07"}}))
-            markers (clj->js [{:latitude  40.806740
-                               :fillKey "FACTORY"
-                               :name "Factory 1"
-                               :radius 10
-                               :fillOpacity 2.0
-                               :longitude -94.671850}
-                              {:latitude  42.806740
-                               :fillKey "FACTORY"
-                               :name "Factory 2"
-                               :radius 10
-                               :fillOpacity 2.0
-                               :longitude -104.671850}
-                              {:latitude  37.806740
-                               :fillKey "FACTORY"
-                               :name "Factory 3"
-                               :radius 10
-                               :fillOpacity 2.0
-                               :longitude -96.671850}
-                              {:latitude  38.806740
-                               :fillKey "FACTORY"
-                               :name "Factory 4"
-                               :radius 10
-                               :fillOpacity 2.0
-                               :longitude -101.671850}
-                              {:latitude  35.806740
-                               :fillKey "MONITOR"
-                               :name "Factory 4"
-                               :radius 6
-                               :fillOpacity 2.0
-                               :longitude -99.671850}
-                              {:latitude  36.806740
-                               :fillKey "MONITOR"
-                               :name "Factory 4"
-                               :radius 6
-                               :fillOpacity 2.0
-                               :longitude -94.671850}
-                              {:latitude  36.806740
-                               :fillKey "MONITOR"
-                               :name "Factory 4"
-                               :radius 6
-                               :fillOpacity 2.0
-                               :longitude -103.671850}
-                              {:latitude  39.806740
-                               :fillKey "MONITOR"
-                               :name "Factory 4"
-                               :radius 6
-                               :fillOpacity 2.0
-                               :longitude -92.671850}
-                              {:latitude  42.806740
-                               :fillKey "MONITOR"
-                               :name "Factory 4"
-                               :radius 6
-                               :fillOpacity 2.0
-                               :longitude -91.671850}
-                              ])]
+            markers (clj->js (vec
+                              (concat
+                               (map
+                                (comp (partial merge monitor-bubble) normalize-coords)
+                                (:monitors @model/mini-db))
+                               (map
+                                (comp (partial merge factory-bubble) normalize-coords)
+                                (:factories @model/mini-db)))))]
         (.bubbles usa-map markers (clj->js {:popupTemplate (fn [geo data]
                                                              (let [data (js->clj data :keywordize-keys true)]
-                                                               (println data)
                                                                (str "<div class=hoverinfo>"
-                                                                    "<span class=font-weight-bold>" (:name data) "</span>"
+                                                                    "<span class=font-weight-bold>" (or (:factory_name data) (:id data)) "</span>"
                                                                     (when-let [desc (:description data)] (str "<br/>" "Description: " desc))
                                                                     "<br/>" (str "Longitude: " (:longitude data))
                                                                     "<br/>" (str "Latitude: "  (:latitude data))
